38a5a173a9ee7b382d8b214db9097d4f4889be42
Uni <a5304186@gmail.com>
2021-06-02 18:40:29 +0800

    CRUD

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,10 @@
 import './assets/scss/index.scss';
 
+let items = [];
+
 function calculateRemainDays(target) {
   const current = new Date();
-  const remainDays = parseInt((new Date(target) - current) / 1000 / 60 / 60 / 24);
+  const remainDays = parseInt((target - current) / 1000 / 60 / 60 / 24);
 
 
   return remainDays;
@@ -21,35 +23,86 @@ function getRandomColor() {
   return `#${color.join('')}`;
 }
 
-function addNewEvent() {
+function addEventItem() {
   const data = {
     title: $('#title').val(),
     date: $('#deadline').val(),
-    day: calculateRemainDays($('#deadline').val())
-  }
+    day: calculateRemainDays(new Date($('#deadline').val())),
+    color: getRandomColor()
+  };
 
   if (data.title && data.date) {
-    $('.list').append(`<div class="list__item" style="border-color: ${getRandomColor()}">
+    items.push(data);
+    appendItemToList(data);
+
+    console.log(items);
+  }
+}
+
+function appendItemToList(data) {
+  $('.list').append(`<div class="list__item" style="border-color: ${data.color}">
                         <div class="list__item--title">
                           <p>${data.title}</p>
                           <div class="timer days"><b>${data.day}</b> days left</div>
                         </div>
                         <div class="btn">
+                          <button id="edit">edit</button>
                           <button id="delete">delete</button>
                         </div>
                       </div>`);
-  }
-
 }
+
+function deleteEventItem(index) {
+  items.splice(index, 1);
+}
+
+function updateEventItem(index) {
+  items[index] = {
+    title: $('#update #title').val(),
+    date: $('#update #deadline').val(),
+    days: calculateRemainDays(new Date($('#update #deadline').val())),
+    color: items[index].color
+  };
+}
+
+$(document).ready(function () {
+  $('#title').val('');
+  $('#deadline').val('');
+});
 
 // add item
 $('#submit').click(function () {
-  addNewEvent();
+  addEventItem();
   $('#title').val('');
   $('#deadline').val('');
 });
 
 // delete item
 $(document).on('click', '#delete', function () {
+  deleteEventItem($(this).parent().parent().index());
   $(this).parent().parent().remove();
+});
+
+// edit item
+$(document).on('click', '#edit', function () {
+  const content = $(this).parent().siblings();
+  const index = $(this).parent().parent().index();
+
+  if ($(this).hasClass('edit')) {
+    updateEventItem(index);
+    $(this).removeClass('edit');
+    $(this).text('edit');
+    content.html(`<div class="list__item--title">
+                    <p>${items[index].title}</p>
+                    <div class="timer days"><b>${items[index].days}</b> days left</div>
+                  </div>`);
+
+  } else {
+    $(this).addClass('edit');
+    $(this).text('update');
+    content.html(`<form id="update">
+                    <input type="text" id="title" name="eventTitle" value="${items[index].title}">
+                    <input type="date" id="deadline" name="eventDate" value="${items[index].date}">
+                  </form>`);
+  }
 });
